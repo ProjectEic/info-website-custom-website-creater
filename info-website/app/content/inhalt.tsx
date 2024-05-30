61177e8d5b76c1180826fd370329b71b4a5da735
Luca Schirmer <66419654+LucaSchirmer@users.noreply.github.com>
2024-05-30 21:49:50 +0200

    init react....

---
+++ info-website/app/content/inhalt.tsx
@@ -0,0 +1,24 @@
+export default function Inhalt() {
+  return (
+      <section id="Inhalt"> 
+        <h3>Inhalt</h3>
+        <p>Keiner der Unterpunkte (außer der Kontaktbereich) ist verpflichtend.</p>
+        <ol>
+            <li>
+                Startseite: 
+                <ol>
+                    <li>Firmenname/Name der Oragnisation</li>
+                    <li>Beschreibung der Tätigkeit/Nähere Infos über die zugrunde liegende Website (am besten 2-4 Sätze).</li>
+                </ol>
+            </li>
+            <li>
+                Dienstleistungen: 
+                <ol>
+                    <li>Name der Dienstleistung</li>
+                    <li>Beschreibung der Dienstleistung: Was bietet man an? Was kostet es? Wie lange dauert es?</li>
+                </ol>
+            </li>
+        </ol> 
+      </section> 
+    );
+}
---
+++ info-website/app/page.tsx
@@ -0,0 +1,21 @@
+import Inhalt from './content/inhalt';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h2>SchirmerRinke Software Info-Radgeber</h2>
+
+      <div className="flex flex-col gap-4">
+        <p>In diesem kurzen Infobeitrag erfährst du alles, was du über deine Website wissen musst.</p>
+        <ol className="list-decimal">
+          <li><a href="#Inhalt">Inhalt</a></li>
+          <li><a href="#Bilder">Bilder</a></li>
+          <li><a href="#Rechtliches">Rechtliches</a></li>
+          <li><a href="#Kontakt">Kontakt</a></li>
+        </ol>
+      </div>
+      
+      <Inhalt />
+    </main>
+  );
+}
