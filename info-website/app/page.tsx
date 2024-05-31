4f6876e1e1bfa6c4921578d2f3d27a1f606cbba3
Luca Schirmer <66419654+LucaSchirmer@users.noreply.github.com>
2024-05-31 23:23:37 +0200

    Update page.tsx

--- info-website/app/page.tsx
+++ info-website/app/page.tsx
@@ -3,11 +3,11 @@ import Inhalt from './content/inhalt';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>SchirmerRinke Software Info-Radgeber</h2>
+      <h2 className="text-2xl">SchirmerRinke Software Info-Radgeber</h2>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 ">
         <p>In diesem kurzen Infobeitrag erfährst du alles, was du über deine Website wissen musst.</p>
-        <ol className="list-decimal">
+        <ol className="list-decimal text-2xl">
           <li><a href="#Inhalt">Inhalt</a></li>
           <li><a href="#Bilder">Bilder</a></li>
           <li><a href="#Rechtliches">Rechtliches</a></li>
