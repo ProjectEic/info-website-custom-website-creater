7c6bbf61447a6c1054df961b00a6ad6118b8b35c
Luca Schirmer <acu1410l@gmail.com>
2024-06-04 21:30:48 +0200

    Update kontakt.tsx
    Co-Authored-By: Jakob Rinke <61539310+JakobRinke@users.noreply.github.com>

--- info-website/app/content/kontakt.tsx
+++ info-website/app/content/kontakt.tsx
@@ -3,6 +3,7 @@ export default function Kontakt() {
         <section id="Kontakt" className="text-[#434656] w-screen flex items-center flex-col h-[110vh] gap-24 p-24"> 
             <div>
                 <h3 className="font-mono text-5xl text-center tracking-[15px]">Kontakt</h3>
+                
             </div>
         </section> 
         );
